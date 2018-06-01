83e6a04b0b3718fbe9f4eca75dee02c6909ca411
Jack DuPlessis <jduplessis294@gmail.com>
2018-05-31 22:52:00 -0400

    Get started with Heroku

---
+++ index.js
@@ -0,0 +1,62 @@
+/* express app as a webtask */
+
+const Express = require('express')
+const app = Express()
+const admin = require('firebase-admin')
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: 'upcrash-server',
+    clientEmail: process.env.EMAIL,
+    privateKey: '-----BEGIN PRIVATE KEY-----\n'+process.env.KEY+'\n-----END PRIVATE KEY-----\n'
+  }),
+  databaseURL: 'https://upcrash-server.firebaseio.com'
+})
+
+const template = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta charset="UTF-8">
+  <title>title</title>
+  </head>
+  <body>
+%HTML%
+  </body>
+  <style>
+%STYLE%
+  </style>
+  <script>
+%SCRIPT%
+  </script>
+  </html>
+`
+
+app.use(require('body-parser').json())
+
+app.get('/:id', function (req, res) {
+  try {
+    let ref = admin.database().ref(req.params.id)
+    ref.on('value', function (snapshot) {
+      let json = snapshot.val()
+      if (!json || !json.html) {
+        res.status(404).end()
+        return
+      }
+      let filledInTemplate = template.replace('%HTML%', json.html)
+                                     .replace('%STYLE%', json.css)
+                                     .replace('%SCRIPT%', json.js)
+      res.type('html')
+      res.send(filledInTemplate)
+    }, function (err) {
+      res.status(404).end()
+      return
+    })
+  } catch (e) {
+    res.status(404).end()
+    return
+  }
+})
+
+// expose this express app as a webtask-compatible function
+app.listen(process.env.PORT || 8000, e => console.log('Running'))
